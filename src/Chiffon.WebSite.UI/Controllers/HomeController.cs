b16529723dedb8ef0f154e7688d2cc29d3977f2c
Chtoucas <chtoucas@narvalo.org>
2013-07-22 23:38:36 +0200

    Initialization of the dynamic part.

---
+++ src/Chiffon.WebSite.UI/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿namespace Chiffon.WebSite.UI.Controllers
+{
+    using System.Web.Mvc;
+
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/Chiffon.WebSite.UI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Chiffon.WebSite.UI.MvcApplication" Language="C#" %>
