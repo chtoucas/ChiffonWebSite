ff078c54a17f275b9bb1b7425cdaadc6169c51ef
Chtoucas <chtoucas@narvalo.org>
2013-07-23 18:14:08 +0200

    Nada

--- src/Chiffon.WebSite.UI/Global.asax.cs
+++ src/Chiffon.WebSite.UI/Global.asax.cs
@@ -1,14 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Http;
-using System.Web.Mvc;
-using System.Web.Optimization;
-using System.Web.Routing;
-
-namespace Chiffon.WebSite.UI
+﻿namespace Chiffon.WebSite.UI
 {
+    using System.Web.Http;
+    using System.Web.Mvc;
+    //using System.Web.Optimization;
+    using System.Web.Routing;
+
     // Remarque : pour obtenir des instructions sur l'activation du mode classique IIS6 ou IIS7, 
     // visitez http://go.microsoft.com/?LinkId=9394801
 
@@ -21,7 +17,7 @@ namespace Chiffon.WebSite.UI
             WebApiConfig.Register(GlobalConfiguration.Configuration);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            //BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
