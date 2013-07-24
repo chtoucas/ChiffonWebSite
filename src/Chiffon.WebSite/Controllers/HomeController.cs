c3544f5e7415d41853f3cbb0855b6b884912a591
Chtoucas <chtoucas@yahoo.it>
2013-07-24 18:29:04 +0200

    Disabled the f# project

---
+++ src/Chiffon.WebSite/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿namespace Chiffon.WebSite.Controllers
+{
+    using System.Web.Mvc;
+
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/Chiffon.WebSite/Controllers/MembersController.cs
@@ -0,0 +1,13 @@
+﻿namespace Chiffon.WebSite.Controllers
+{
+    using System.Web.Mvc;
+
+    public class MembersController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
