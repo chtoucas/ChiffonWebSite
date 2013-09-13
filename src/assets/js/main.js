2bf0af9360012d4ec78b62c7298ff01366d7c4df
Chtoucas <chtoucas@yahoo.it>
2013-09-13 08:33:18 +0200

    Simplification de JS.

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,65 +1,97 @@
 ;
 
-this.Bundles = (function(undef) {
+this.App = (function(win, yepnope, undef) {
   'use strict';
 
-  var Bundles = {
-    baseUrl: '/assets/js/'
+  var debug = false
+    , version = '1.0'
+    , locales = ['fr', 'en']
+    , gaq = 'UA-43374044-1';
 
-    , version: undef
+  return function() {
+    var baseUrl = '/assets/js/';
 
-    , rebase: function(src) { return Bundles.baseUrl + src; }
+    function rebase(src) { return baseUrl + src; }
 
-    , vendor: function(src) { return Bundles.baseUrl + 'vendor/' + src; }
+    function vendor(src) { return baseUrl + 'vendor/' + src; }
 
-    , jQuery: function() {
-      return Bundles.vendor('__proto__' in {} ? 'jquery-2.0.3.min.js' : 'jquery-1.10.2.min.js');
-    }
+    this.baseUrl = function(value) {
+      if (undef === value) {
+        throw new ReferenceError('The "baseUrl" value is undefined.');
+      } else if ('/' !== value.substring(-1, 1)) {
+        baseUrl = value + '/';
+      } else {
+        baseUrl = value;
+      }
+    };
 
-    //, jQueryCookie: function() { return Bundles.vendor('jquery.cookie-1.3.1.min.js'); }
+    this.gaq = function(value) {
+      if (undef === value) {
+        throw new ReferenceError('The "gaq" value is undefined.');
+      }
+      gaq = value;
+    };
 
-    //, jQueryOutside: function() { return Bundles.vendor('jquery.ba-outside-events-1.1.min.js'); }
+    this.version = function(value) {
+      if (undef === value) {
+        throw new ReferenceError('The "version" value is undefined.');
+      }
+      version = value;
+    };
 
-    , jQueryValidate: function(locale) {
+    this.jQuery = function() {
+      return vendor('__proto__' in {} ? 'jquery-2.0.3.min.js' : 'jquery-1.10.2.min.js');
+    };
+
+    //this.jQueryCookie = function() { return vendor('jquery.cookie-1.3.1.min.js'); }
+
+    //this.jQueryOutside = function() { return vendor('jquery.ba-outside-events-1.1.min.js'); }
+
+    this.jQueryValidate = function(locale) {
       return ['jquery.validate.min.js', 'additional-methods.min.js']
         .push('localization/messages_' + locale + '.js')
-        .map(function(src) { return Bundles.vendor('jquery.validate-1.11.1/' + src); });
-    }
+        .map(function(src) { return vendor('jquery.validate-1.11.1/' + src); });
+    };
 
-    , Chiffon: function() {
-      return undef !== Bundles.version
-        ? rebase('chiffon-' + Bundles.version + '.min.js')
-        : ['vendor/l10n-2013.04.18.min.js', 'localization.js', 'chiffon.js'].map(Bundles.rebase);
-    }
-  };
+    this.Chiffon = function() {
+      return debug
+        ? ['vendor/l10n-2013.04.18.min.js', 'localization.js', 'chiffon.js'].map(rebase)
+        : rebase('chiffon-' + version + '.min.js');
+    };
 
-  return Bundles;
-})();
+    this.main = function(locale, fn) {
+      var that = this;
 
-this.main = (function(win, Dependencies, yepnope, undef) {
-  'use strict';
-
-  var locales = ['fr', 'en'];
-
-  return function(locale, fn) {
-    if (-1 === locales.indexOf(locale)) {
-      throw new Error('The locale "' + locale + '" is not supported.');
-    }
-
-    // FIXME: Quid quand un des appels échoue ?
-    yepnope({
-      load: [Bundles.jQuery()].concat(Bundles.Chiffon())
-      , complete: function() {
-        var Chiffon = win.Chiffon;
-        if (undef === Chiffon) { return; }
-
-        var context = {
-          locale: locale
-        };
-
-        fn(new Chiffon(context));
+      if (-1 === locales.indexOf(locale)) {
+        throw new Error('The locale "' + locale + '" is not supported.');
       }
-    });
-  };
 
-})(this, this.Dependencies, this.yepnope);
+      // FIXME: Quid quand un des appels échoue ?
+      yepnope([
+        {
+          // Google Analytics.
+          test: gaq
+          , yep: '//www.google-analytics.com/ga.js'
+          , callback: function() {
+            ga('create', gaq, 'pourquelmotifsimone.com');
+            ga('send', 'pageview');
+          }
+        }, {
+          load: [Bundles.jQuery()].concat(Bundles.Chiffon())
+          , complete: function() {
+            var Chiffon = win.Chiffon;
+            if (undef === Chiffon) { return; }
+
+            var context = {
+              locale: locale
+              , app: that
+            };
+
+            fn(new Chiffon(context));
+          }
+        }
+      ]);
+    };
+  }
+
+})(this, this.yepnope);
