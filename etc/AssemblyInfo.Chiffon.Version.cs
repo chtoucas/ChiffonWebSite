3b817d88d033ff86f823f04241e35ec954b70cbe
Chtoucas <chtoucas@narvalo.org>
2013-09-16 00:06:41 +0200

    Amélioration des scripts MSBuild.

--- etc/AssemblyInfo.Chiffon.Version.cs
+++ etc/AssemblyInfo.Chiffon.Version.cs
@@ -7,13 +7,13 @@ using System.Reflection;
 using System.Diagnostics.CodeAnalysis;
 
 [assembly: AssemblyVersion("1.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.2")]
+[assembly: AssemblyFileVersion("1.0.0.28")]
 [assembly: AssemblyInformationalVersion("1.0.0-RELENG")]
 
 #if DEBUG
-[assembly: AssemblyConfiguration("Debug packaged on 2013-09-15 21:03")]
+[assembly: AssemblyConfiguration("Debug packaged on 2013-09-16 00:04")]
 #else
-[assembly: AssemblyConfiguration("Release packaged on 2013-09-15 21:03")]
+[assembly: AssemblyConfiguration("Release packaged on 2013-09-16 00:04")]
 #endif
 
 [assembly: SuppressMessage("Microsoft.Usage", "CA2243:AttributeStringLiteralsShouldParseCorrectly")]
