68205513f5623904560bb2ca9969c10b38573d03
Chtoucas <chtoucas@narvalo.org>
2013-09-18 08:49:56 +0200

    L'enreigstrement devient moins contraignant.

--- etc/AssemblyInfo.Version.cs
+++ etc/AssemblyInfo.Version.cs
@@ -6,14 +6,14 @@ using System;
 using System.Reflection;
 using System.Diagnostics.CodeAnalysis;
 
-[assembly: AssemblyVersion("1.0.22.0")]
-[assembly: AssemblyFileVersion("1.0.22.0")]
-[assembly: AssemblyInformationalVersion("1.0.22")]
+[assembly: AssemblyVersion("1.0.23.0")]
+[assembly: AssemblyFileVersion("1.0.23.0")]
+[assembly: AssemblyInformationalVersion("1.0.23")]
 
 #if DEBUG
-[assembly: AssemblyConfiguration("Debug packaged on 2013-09-18 08:05")]
+[assembly: AssemblyConfiguration("Debug packaged on 2013-09-18 08:43")]
 #else
-[assembly: AssemblyConfiguration("Release packaged on 2013-09-18 08:05")]
+[assembly: AssemblyConfiguration("Release packaged on 2013-09-18 08:43")]
 #endif
 
 [assembly: SuppressMessage("Microsoft.Usage", "CA2243:AttributeStringLiteralsShouldParseCorrectly")]
--- src/ViewModels/RegisterViewModel.cs
+++ src/ViewModels/RegisterViewModel.cs
@@ -11,15 +11,15 @@
         public string EmailAddress { get; set; }
 
         [Required(ErrorMessage = "Le champs \"Prénom\" est obligatoire.")]
-        [StringLength(50, MinimumLength = 5, ErrorMessage = "Le champs \"Prénom\" doit comporter au moins 5 caractères.")]
+        [StringLength(50, MinimumLength = 2, ErrorMessage = "Le champs \"Prénom\" doit comporter au moins 2 caractères.")]
         public string Firstname { get; set; }
 
         [Required(ErrorMessage = "Le champs \"Nom\" est obligatoire.")]
-        [StringLength(50, MinimumLength = 5, ErrorMessage = "Le champs \"Nom\" doit comporter au moins 5 caractères.")]
+        [StringLength(50, MinimumLength = 2, ErrorMessage = "Le champs \"Nom\" doit comporter au moins 2 caractères.")]
         public string Lastname { get; set; }
 
         [Required(ErrorMessage = "Le champs \"Agence\" est obligatoire.")]
-        [StringLength(100, MinimumLength = 1, ErrorMessage = "Le champs \"Agence\" doit comporter au moins 5 caractères.")]
+        [StringLength(100, MinimumLength = 2, ErrorMessage = "Le champs \"Agence\" doit comporter au moins 2 caractères.")]
         public string CompanyName { get; set; }
 
         public string Message { get; set; }
