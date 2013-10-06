b2bbf4ed881656f236ad26d354a8be8becec6cf9
Chtoucas <chtoucas@narvalo.org>
2013-10-06 13:37:00 +0200

    Correctly produce tests when calling Package target.
    The test project was not compiled.

--- etc/AssemblyInfo.Version.cs
+++ etc/AssemblyInfo.Version.cs
@@ -1,14 +1,14 @@
 /*
  * THIS FILE IS GENERATED, ANY LOCAL CHANGES WILL BE LOST.
- * Timestamp: 2013-10-04 18:44 +02:00
+ * Timestamp: 2013-10-06 13:33 +02:00
  */
 
 using System;
 using System.Reflection;
 using System.Diagnostics.CodeAnalysis;
 
-[assembly: AssemblyVersion("1.0.27.273")]
-[assembly: AssemblyFileVersion("1.0.27.273")]
-[assembly: AssemblyInformationalVersion("1.0.27")]
+[assembly: AssemblyVersion("1.1.0.16")]
+[assembly: AssemblyFileVersion("1.1.0.16")]
+[assembly: AssemblyInformationalVersion("1.1.0")]
 
 //[assembly: SuppressMessage("Microsoft.Usage", "CA2243:AttributeStringLiteralsShouldParseCorrectly")]
