a798c79f437e6c0fbaa1f58e8f4e76c9f836c895
Chtoucas <chtoucas@narvalo.org>
2013-10-14 00:23:27 +0200

    Cleaned up the deploy script.

--- miscs/scripts/default.ps1
+++ miscs/scripts/default.ps1
@@ -1,17 +1,22 @@
 # MSDeploy -verb:sync -source:appHostConfig="XXX" -dest:auto -enableRule:AppOffline
+# http://blog.mdavies.net/2012/08/12/microsofts-hidden-gem-msdeploy/
+# MSDeploy retryInterval ?
 
 Properties {
-  $scriptPath = $(Get-Location).Path
+  $basePath = $(Get-Location).Path
 
-  $backupDir = "$scriptPath\_backup"
-  $configPath = "$scriptPath\Deploy.config"
-
-  [xml] $configXml = Get-Content -Path $configPath
-
+  [xml] $configXml = Get-Content -Path "$basePath\Deploy.config"
   [System.Xml.XmlElement] $config = $configXml.configuration
 
-  $webSite = $config.WebSite
-  $assets  = $config.Assets
+  $SourceDir = $basePath
+  $BackupDir = "$basePath\_backup"
+  $Logfile = "$basePath\deploy.log"
+
+  $WebSiteConfig = $config.WebSite
+  $AssetsConfig  = $config.Assets
+
+  $DeployWebSite = $WebSiteConfig.GetAttribute("deploy") -eq "true"
+  $DeployAssets = $AssetsConfig.GetAttribute("deploy") -eq "true"
 }
 
 Task default -depends Help
@@ -20,56 +25,90 @@ Task Help {
   Write-Host "Sorry, help not yet available..."
 }
 
-Task Deploy -depends DeployWebSite
+Task Backup -depends BackupWebSite, BackupAssets
 
-Task Rollback -depends RollbackWebSite
+Task Deploy -depends DeployWebSite, DeployAssets
 
-Task DeployWebSite -depends BackupWebSite `
-  -precondition { $webSite.GetAttribute("deploy") -eq "true" } {
+Task Rollback -depends RollbackWebSite, RollbackAssets
 
-  Publish-WebSite -Website $webSite.IISWebSite `
-    -Source "$scriptPath\$($webSite.SourceRelativePath)" `
-    -Destination $webSite.IISPhysicalPath
-}
 
 Task BackupWebSite -depends CreateBackupDirectory {
-  $backupPackage = "$backupDir\$($webSite.BackupPackage)"
-
-  if (Test-Path $backupPackage) {
-    Write-ColoredOutput "Backup skipped, it already exists..."  -foregroundcolor "Yellow"
-    Return
-  }
-
-  Backup-WebSite -Website $webSite.IISWebSite -BackupPackage $backupPackage
+  Backup-FromConfig -Config $WebSiteConfig
 }
 
-Task RollbackWebSite {
-  $backupPackage = "$backupDir\$($webSite.BackupPackage)"
-
-  if (-not(Test-Path $backupPackage)) {
-    Write-ColoredOutput "Backup package does not exist..."  -foregroundcolor "Yellow"
-    Return
-  }
-
-  Rollback-WebSite -Website $webSite.IISWebSite -BackupPackage $backupPackage
+Task DeployWebSite -depends BackupWebSite -precondition { $DeployWebSite } {
+  Publish-FromConfig -Config $WebSiteConfig
 }
 
-Task DeployAssets -depends BackupAssets `
-  -precondition { $assets.GetAttribute("deploy") -eq "true" } {
+Task RollbackWebSite -precondition { $DeployWebSite } {
+  Rollback-FromConfig -Config $WebSiteConfig
 }
+
 
 Task BackupAssets -depends CreateBackupDirectory {
+  Backup-FromConfig -Config $AssetsConfig
 }
 
-Task RollbackAssets -depends ReadConfig {
+Task DeployAssets -depends BackupAssets -precondition { $DeployAssets } {
+  Publish-FromConfig -Config $AssetsConfig
 }
+
+Task RollbackAssets -precondition { $DeployAssets } {
+  Rollback-FromConfig -Config $AssetsConfig
+}
+
 
 Task CreateBackupDirectory {
-  if (Test-Path $backupDir) {
+  if (Test-Path $BackupDir) {
     return
   }
 
-  New-Item $backupDir -Type directory -ErrorAction SilentlyContinue | Out-Null
+  New-Item $BackupDir -Type directory -ErrorAction SilentlyContinue | Out-Null
+}
+
+
+# Utilitaires
+
+function Get-BackupPackage {
+  param([System.Xml.XmlElement] $config)
+
+  "$BackupDir\$($config.BackupPackage)"
+}
+
+function Backup-FromConfig {
+  param([System.Xml.XmlElement] $config)
+
+  $package = Get-BackupPackage -Config $config
+  Backup-WebSite -Website $config.IISWebSite -ToPackage $package
+}
+
+function Publish-FromConfig {
+  param([System.Xml.XmlElement] $config)
+
+  $source = "$SourceDir\$($config.SourceRelativePath)"
+  Publish-WebSite -Website $config.IISWebSite -Source $source -Destination $config.IISPhysicalPath
+}
+
+function Rollback-FromConfig {
+  param([System.Xml.XmlElement] $config)
+
+  $package = Get-BackupPackage -Config $config
+  Rollback-WebSite -Website $config.IISWebSite -FromPackage $package
+}
+
+function Backup-WebSite {
+  param(
+    [string] $webSite,
+    [string] $toPackage
+  )
+
+  if (Test-Path $toPackage) {
+    Write-ColoredOutput "Skipping backup for '$website', the package already exists..."  -foregroundcolor "Gray"
+    Return
+  }
+
+  Write-ColoredOutput "Backing up '$webSite'..." -foregroundcolor "Yellow"
+  Exec { MSDeploy "-verb:sync", "-source:appHostConfig=$webSite" "-dest:package=$toPackage" >> $Logfile }
 }
 
 function Publish-WebSite {
@@ -79,42 +118,32 @@ function Publish-WebSite {
     [string] $destination
   )
 
-  Write-ColoredOutput "Stopping $webSite..." -foregroundcolor "Yellow"
+  Write-ColoredOutput "Stopping '$webSite'..." -foregroundcolor "Yellow"
   Stop-WebSite -Name $webSite
 
-  Exec { MSDeploy "-verb=sync", "-source=dirPath=$source", "-dest=dirPath=$destination" }
-  #if ($LastExitCode -ne 0) { throw "MSDeploy failed." }
+  Write-ColoredOutput "Publishing '$webSite'..." -foregroundcolor "Yellow"
+  Exec { MSDeploy "-verb=sync", "-source=dirPath=$source", "-dest=dirPath=$destination", "-useCheckSum" >> $Logfile  }
 
-  Write-ColoredOutput "Starting $webSite..." -foregroundcolor "Yellow"
+  Write-ColoredOutput "Starting '$webSite'..." -foregroundcolor "Yellow"
   Start-WebSite -Name $webSite
-}
-
-function Backup-WebSite {
-  param(
-    [string] $webSite,
-    [string] $backupPackage
-  )
-
-  Write-ColoredOutput "Backing up $webSite..." -foregroundcolor "Yellow"
-
-  Exec { MSDeploy "-verb:sync", "-source:appHostConfig=$webSite" "-dest:package=$backupPackage" }
-  #if ($LastExitCode -ne 0) { throw "MSDeploy failed." }
 }
 
 function Rollback-WebSite {
   param(
     [string] $webSite,
-    [string] $backupPackage
+    [string] $fromPackage
   )
 
-  Write-ColoredOutput "Stopping $webSite..." -foregroundcolor "Yellow"
+  if (-not(Test-Path $fromPackage)) {
+    throw "Can not rollback, '$fromPackage' does not exist..."
+  }
+
+  Write-ColoredOutput "Stopping '$webSite'..." -foregroundcolor "Yellow"
   Stop-WebSite -Name $webSite
 
-  Write-ColoredOutput "Rolling back $webSite..." -foregroundcolor "Yellow"
+  Write-ColoredOutput "Rolling back '$webSite'..." -foregroundcolor "Yellow"
+  Exec { MSDeploy "-verb:sync", "-source:package=$fromPackage", "-dest:appHostConfig=$webSite", "-useCheckSum" >> $Logfile }
 
-  Exec { MSDeploy "-verb:sync", "-source:package=$backupPackage", "-dest:appHostConfig=$webSite" }
-  #if ($LastExitCode -ne 0) { throw "MSDeploy failed." }
-
-  Write-ColoredOutput "Starting $webSite..." -foregroundcolor "Yellow"
+  Write-ColoredOutput "Starting '$webSite'..." -foregroundcolor "Yellow"
   Start-WebSite -Name $webSite
 }
