de2a7baf28d77c1b543c94dd5f9eb1e83b49f1ee
Chtoucas <chtoucas@narvalo.org>
2013-10-17 22:34:12 +0200

    Several bugfixes after last changes.
    There are still two problems to solve:
    * NuGet.exe is still in the source tree
    * Narvalo.Build.tasks is referenced with a version dependent path
    * Last version of YUICompressor does not seem to like Windows paths

--- build.ps1
+++ build.ps1
@@ -5,7 +5,7 @@ $scriptDir = Split-Path $scriptPath
 
 get-module psake | remove-module
 
-.nuget\NuGet.exe install .nuget\packages.config -OutputDirectory packages
+.\tools\NuGet.exe install .nuget\packages.config -OutputDirectory packages
 import-module (Get-ChildItem "$scriptDir\packages\psake.*\tools\psake.psm1" | Select-Object -First 1)
 
 exec { invoke-psake "$scriptDir\scripts\build\build.ps1" $task }
