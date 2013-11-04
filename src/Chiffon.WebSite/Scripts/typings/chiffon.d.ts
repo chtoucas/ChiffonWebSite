b840c70dcebc7c850fb72365b99ea3f47c421f7b
Chtoucas <chtoucas@narvalo.org>
2013-11-04 17:43:44 +0100

    Added intellisense for JS and experiments with Typescript.

---
+++ src/Chiffon.WebSite/Scripts/typings/chiffon.d.ts
@@ -0,0 +1,35 @@
+﻿
+/// <reference path="jquery/jquery.d.ts"/>
+
+interface WatermarkOptions {
+  wrapperStart?: string;
+  wrapperEnd?: string;
+}
+
+interface ModalOptions {
+  clickClose?: boolean;
+  closeText?: string;
+  escapeClose?: boolean;
+  modalClass?: string;
+  showClose?: boolean;
+}
+
+interface Modal {
+  block(): void;
+  center(): void;
+  close(): void;
+  hide(): void;
+  open(): void;
+  show(): void;
+  unblock(): void;
+}
+
+interface JQuery {
+  external(): any;
+  modal(options?: ModalOptions): any;
+  watermark(watermark?: string, options?: WatermarkOptions): any;
+}
+
+interface JQueryStatic {
+  modal(elmt: any, options?: ModalOptions): Modal;
+}
