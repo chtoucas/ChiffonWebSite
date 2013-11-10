ea875b57eb78245d277ea85ff61f90b35c413858
Chtoucas <chtoucas@narvalo.org>
2013-11-10 13:05:58 +0100

    Now Gruntfile follows the new style guidelines.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,12 +1,12 @@
-/*jshint node:true, indent:false*/
+/*jshint node:true*/
 /*jslint node:true*/
 
 module.exports = function(grunt) {
   'use strict';
 
   var config = {
-    projectDir: './src/Chiffon.WebSite/assets'
-    , reportsDir: __dirname + '/_work/reports'
+    projectDir: './src/Chiffon.WebSite/assets',
+    reportsDir: __dirname + '/_work/reports'
   };
 
   function mapCss(value) { return config.projectDir + '/css/' + value; }
@@ -27,206 +27,206 @@ module.exports = function(grunt) {
   }
 
   grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json')
+    pkg: grunt.file.readJSON('package.json'),
 
-    , version: readSemVer()
+    version: readSemVer(),
 
-    , sources: {
+    sources: {
       // Fichiers CSS à analyser.
       css: [
-        '01-chiffon.base.css'
-        , '02-chiffon.helpers.css'
-        , '03-chiffon.css'
-      ].map(mapCss)
+        '01-chiffon.base.css',
+        '02-chiffon.helpers.css',
+        '03-chiffon.css'
+      ].map(mapCss),
       // Fichiers JavaScript à analyser.
-      , js: [
-        'chiffon.jquery.js'
-        , 'chiffon.main.js'
-        , 'chiffon.localization.js'
-        , 'chiffon.core.js'
+      js: [
+        'chiffon.jquery.js',
+        'chiffon.main.js',
+        'chiffon.localization.js',
+        'chiffon.core.js'
       ].map(mapJs).concat('Gruntfile.js')
-    }
+    },
 
-    , bundles: {
+    bundles: {
       // Groupes CSS.
       css: {
         // Groupe CSS pour les écrans.
         screen: {
           src: [
-            'normalize-2.1.3.css'
-            , '01-chiffon.base.css'
-            , '02-chiffon.helpers.css'
-            , '03-chiffon.css'
-          ].map(mapCss)
-          , dest: mapCss('_screen-<%= version %>.css')
+            'normalize-2.1.3.css',
+            '01-chiffon.base.css',
+            '02-chiffon.helpers.css',
+            '03-chiffon.css'
+          ].map(mapCss),
+          dest: mapCss('_screen-<%= version %>.css')
         }
-      }
+      },
       // Groupes JavaScript.
-      , js: {
+      js: {
         // Groupe JavaScript de démarrage.
         chiffon_main: {
-          src: ['vendor/yepnope-1.5.4.js', 'chiffon.main.js'].map(mapJs)
-          , dest: mapJs('_main-<%= version %>.js')
-          , srcmap: '_main-<%= version %>.map'
-        }
+          src: ['vendor/yepnope-1.5.4.js', 'chiffon.main.js'].map(mapJs),
+          dest: mapJs('_main-<%= version %>.js'),
+          srcmap: '_main-<%= version %>.map'
+        },
         // Groupe JavaScript des librairies externes.
-        , libraries: {
-          src: ['vendor/jquery-2.0.3.min.js', 'vendor/lodash-2.2.1.min.js'].map(mapJs)
-          , dest: mapJs('_lib-<%= version %>.js')
-        }
+        libraries: {
+          src: ['vendor/jquery-2.0.3.min.js', 'vendor/lodash-2.2.1.min.js'].map(mapJs),
+          dest: mapJs('_lib-<%= version %>.js')
+        },
         // Groupe JavaScript chargé en différé.
-        , chiffon_core: {
+        chiffon_core: {
           src: [
-            'vendor/l10n-2013.09.12.js'
-            , 'jquery.modal.js'
-            , 'chiffon.jquery.js'
-            , 'chiffon.localization.js'
-            , 'chiffon.core.js'
-          ].map(mapJs)
-          , dest: mapJs('_core-<%= version %>.js')
-          , srcmap: '_core-<%= version %>.map'
+            'vendor/l10n-2013.09.12.js',
+            'jquery.modal.js',
+            'chiffon.jquery.js',
+            'chiffon.localization.js',
+            'chiffon.core.js'
+          ].map(mapJs),
+          dest: mapJs('_core-<%= version %>.js'),
+          srcmap: '_core-<%= version %>.map'
         }
       }
-    }
+    },
 
-    , nobom: {
-      js: { src: '<%= sources.js %>' }
-      , css: { src: '<%= sources.css %>' }
-    }
+    nobom: {
+      js: { src: '<%= sources.js %>' },
+      css: { src: '<%= sources.css %>' }
+    },
 
     /*
      * Analyse des fichiers CSS via CSSLint.
      */
-    , csslint: {
+    csslint: {
       // NB: Chaque fichier contient ses propres instructions d'analyse.
       chiffon: {
-        options: { formatters: [ {id: 'text', dest: mapLog('csslint.log')} ] }
-        , src: '<%= sources.css %>'
+        options: { formatters: [ {id: 'text', dest: mapLog('csslint.log')} ] },
+        src: '<%= sources.css %>'
       }
-    }
+    },
 
     /*
      * Minification des fichiers JavaScript via Clean-CSS.
      */
-    , cssmin: {
+    cssmin: {
       chiffon: {
         options: {
           //banner: '/*! Generated on <%= grunt.template.today("yyyy-mm-dd HH:mm") %> */'
-          keepSpecialComments: 0
-          , report: 'min'
-        }
-        , files: { '<%= bundles.css.screen.dest %>': '<%= bundles.css.screen.src %>' }
+          keepSpecialComments: 0,
+          report: 'min'
+        },
+        files: { '<%= bundles.css.screen.dest %>': '<%= bundles.css.screen.src %>' }
       }
-    }
+    },
 
     /*
      * Analyse des fichiers JavaScript via JSHint.
      */
-    , jshint: {
+    jshint: {
       options: {
         // Règles strictes.
-        bitwise: true             // Peut être désactivé localement.
-        //, camelcase: true
-        , curly: true
-        , eqeqeq: true
-        , es3: true
-        , forin: true
-        , freeze: true
-        , immed: true
-        , indent: 2             // Incompatible avec laxcomma et laxbreak ?
-        , latedef: true
-        //, maxcomplexity: true
-        , maxdepth: 2
-        //, maxlen: 1000
-        , maxparams: 3            // Peut être désactivé localement.
-        //, maxstatements: 10
-        , newcap: true
-        , noarg: true
-        , noempty: true
-        , nonew: true
-        //, plusplus: true        // Aucune raison de ne pas utiliser les opérateurs ++ et --.
-        , quotmark: 'single'
-        , strict: true
-        , trailing: true
-        , undef: true
-        , unused: true
+        bitwise: true,           // Peut être désactivé localement.
+        //camelcase: true,
+        curly: true,
+        eqeqeq: true,
+        es3: true,
+        forin: true,
+        freeze: true,
+        immed: true,
+        indent: 2,               // WARNING: Incompatible avec laxcomma et laxbreak ?
+        latedef: true,
+        //maxcomplexity: true,
+        maxdepth: 2,
+        //maxlen: 1000,
+        maxparams: 3,            // Peut être désactivé localement.
+        //maxstatements: 10,
+        newcap: true,
+        noarg: true,
+        noempty: true,
+        nonew: true,
+        //plusplus: true,        // Aucune raison de ne pas utiliser les opérateurs ++ et --.
+        quotmark: 'single',
+        strict: true,
+        trailing: true,
+        undef: true,
+        unused: true
 
         // Règles désactivées.
-        , laxcomma: true
-        , laxbreak:true
-
-        // Environnement
-        //, browser:true
-      }
+        //laxcomma: true,
+        //laxbreak: true,
+        //browser: true
+      },
       // NB: Chaque fichier contient ses propres instructions d'analyse.
-      , files: '<%= sources.js %>'
-    }
+      files: '<%= sources.js %>'
+    },
 
     /*
      * Analyse des fichiers JavaScript via JSLint.
      */
-    /* , jslint: {
+    /*
+    jslint: {
       chiffon: {
         options: {
-          log: mapLog('jslint.log')
-          , errorsOnly: true
-          , failOnError: false
-        }
-        , directives: {
-          nomen: true
-          , white: true
-          , todo: true
-          , plusplus: true
-          , browser: true
-        }
-        , src: '<%= sources.js %>'
+          log: mapLog('jslint.log'),
+          errorsOnly: true,
+          failOnError: false
+        },
+        directives: {
+          nomen: true,
+          white: true,
+          todo: true,
+          plusplus: true,
+          browser: true
+        },
+        src: '<%= sources.js %>'
       }
-    } */
+    },
+    */
 
     /*
      * Fusion des fichiers JavaScript.
      */
-    , concat: {
+    concat: {
       options: {
-        stripBanners: true
-        , block: true
-        , line: true
+        stripBanners: true,
+        block: true,
+        line: true
+      },
+      libraries: {
+        src: '<%= bundles.js.libraries.src %>',
+        dest: '<%= bundles.js.libraries.dest %>'
       }
-      , libraries: {
-        src: '<%= bundles.js.libraries.src %>'
-        , dest: '<%= bundles.js.libraries.dest %>'
-      }
-    }
+    },
 
     /*
      * Minification des fichiers JavaScript via UglifyJS.
      */
-    , uglify: {
+    uglify: {
       options: {
         // TODO: On ne peut pas combiner les options banner et minification.
         //banner: '/*! Generated on <%= grunt.template.today("yyyy-mm-dd HH:mm") %> */\n'
         // TODO: Vérifier les options.
-        compress: { unused: true }
-        , mangle: true
-        , sourceMapPrefix: 5
-      }
-      , chiffon_main: {
+        compress: { unused: true },
+        mangle: true,
+        sourceMapPrefix: 5
+      },
+      chiffon_main: {
         options: {
-          preserveComments: false
-          , sourceMap: mapJs('<%= bundles.js.chiffon_main.srcmap %>')
-          , sourceMappingURL: '<%= bundles.js.chiffon_main.srcmap %>'
-        }
-        , files: {
+          preserveComments: false,
+          sourceMap: mapJs('<%= bundles.js.chiffon_main.srcmap %>'),
+          sourceMappingURL: '<%= bundles.js.chiffon_main.srcmap %>'
+        },
+        files: {
           '<%= bundles.js.chiffon_main.dest %>' : '<%= bundles.js.chiffon_main.src %>'
         }
-      }
-      , chiffon_core: {
+      },
+      chiffon_core: {
         options: {
-          preserveComments: false
-          , sourceMap: mapJs('<%= bundles.js.chiffon_core.srcmap %>')
-          , sourceMappingURL: '<%= bundles.js.chiffon_core.srcmap %>'
-        }
-        , files: {
+          preserveComments: false,
+          sourceMap: mapJs('<%= bundles.js.chiffon_core.srcmap %>'),
+          sourceMappingURL: '<%= bundles.js.chiffon_core.srcmap %>'
+        },
+        files: {
           '<%= bundles.js.chiffon_core.dest %>': '<%= bundles.js.chiffon_core.src %>'
         }
       }
