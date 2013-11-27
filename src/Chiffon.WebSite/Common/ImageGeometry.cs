a5988e71ad54a14a7e4e37647261364418c2e286
Chtoucas <chtoucas@narvalo.org>
2013-11-27 17:20:46 +0100

    Typo on thumbnails dimension.

--- src/Chiffon.WebSite/Common/ImageGeometry.cs
+++ src/Chiffon.WebSite/Common/ImageGeometry.cs
@@ -6,6 +6,6 @@
         public const int PatternWidth = 700;
         public const int PatternHeight = 520;
         public const int PreviewWidth = 220;
-        public const int PreviewHeight = 100;
+        public const int PreviewHeight = 160;
     }
 }
