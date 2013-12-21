69fbe40ae5c8ac6541b01f06976301a9b704753a
Chtoucas <chtoucas@narvalo.org>
2013-12-21 12:30:41 +0100

    Release 1.10

--- etc/AssemblyInfo.Version.cs
+++ etc/AssemblyInfo.Version.cs
@@ -1,6 +1,6 @@
 /*
  * THIS FILE IS GENERATED, ANY LOCAL CHANGES WILL BE LOST.
- * Timestamp: 2013-12-21 12:22 +01:00
+ * Timestamp: 2013-12-21 12:29 +01:00
  */
 
 using System;
@@ -8,9 +8,9 @@ using System.Reflection;
 using System.Diagnostics.CodeAnalysis;
 
 // Version utilisée par le runtime.
-[assembly: AssemblyVersion("1.10.0.0")]
+[assembly: AssemblyVersion("1.10.0.1")]
 // Version visible dans l'explorateur.
-[assembly: AssemblyFileVersion("1.10.0.0")]
+[assembly: AssemblyFileVersion("1.10.0.1")]
 [assembly: AssemblyInformationalVersion("1.10.0")]
 
 //[assembly: SuppressMessage("Microsoft.Usage", "CA2243:AttributeStringLiteralsShouldParseCorrectly")]
