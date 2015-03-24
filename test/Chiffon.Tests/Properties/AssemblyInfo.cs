4d777e6cce76f1d3b887cd900b27f27217831d69
chtoucas <chtoucas@narvalo.org>
2015-03-24 07:30:59 +0100

    Removed the test project

--- test/Chiffon.Tests/Properties/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("Chiffon.Tests")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyCulture("")]
-
-[assembly: Guid("be5b2bd5-f55e-4ff6-83a0-5212ae75b0d9")]
-
